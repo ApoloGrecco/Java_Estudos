f82347830cfcd275ee1a7de8ac33e17315afb92c
ApoloGrecco <apolo.argolo@gmail.com>
2022-06-03 21:44:44 -0300

    Módulo 5 - Estrutura Repetitivas

---
+++ src/modulo6_exercicios/exercicio_2.java
@@ -0,0 +1,50 @@
+package modulo6_exercicios;
+
+import java.util.Scanner;
+
+public class exercicio_2 {
+
+    public static void main(String[] args) {
+
+        /*
+            Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos
+            no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo
+            será encerrado quando pelo menos uma de duas coordenadas for NULA
+            (nesta situação sem escrever mensagem alguma).
+         */
+
+        Scanner sc = new Scanner(System.in);
+        int valor_x, valor_y;
+
+        System.out.println("Escreva a coordenada de X: ");
+        valor_x = sc.nextInt();
+
+        System.out.println("Escreva a coordenada de Y: ");
+        valor_y = sc.nextInt();
+
+        while (valor_x != 0 && valor_y != 0){
+
+            if (valor_x > 0 && valor_y > 0){
+                System.out.println("Primeiro\n");
+
+            }else if(valor_x < 0 && valor_y > 0) {
+                System.out.println("Segundo\n");
+
+            }else if(valor_x < 0) {
+                System.out.println("Terceiro\n");
+
+            }else{
+                System.out.println("Quarto\n");
+            }
+
+            System.out.println("Escreva a coordenada de X: ");
+            valor_x = sc.nextInt();
+
+            System.out.println("Escreva a coordenada de Y: ");
+            valor_y = sc.nextInt();
+
+        }
+
+        sc.close();
+    }
+}
---
+++ src/modulo6_exercicios/exercicio_3.java
@@ -0,0 +1,56 @@
+package modulo6_exercicios;
+
+import java.util.Scanner;
+
+public class exercicio_3 {
+
+    public static void main(String[] args) {
+
+        /*
+            Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes.
+            Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma:
+            1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4)
+            deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código
+            informado for o número 4. Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que
+            abasteceram cada tipo de combustível, conforme exemplo.
+        */
+
+        Scanner sc = new Scanner(System.in);
+        int alcool = 0, gasolina = 0, diesel = 0, opc;
+
+        System.out.println("Selecione qual seu combustível de preferencia");
+        System.out.println("1 - Álcool");
+        System.out.println("2 - Gasolina");
+        System.out.println("3 - Diesel");
+        System.out.println("4 - Fim\n");
+
+        System.out.print("Selecione uma opção: ");
+        opc = sc.nextInt();
+
+        while(opc != 4) {
+            if (opc == 1) {
+                alcool += 1;
+            }
+            else if (opc == 2) {
+                gasolina += 1;
+            }
+            else if (opc == 3) {
+                diesel += 1;
+            }
+
+            System.out.print("Selecione uma opção: ");
+            opc = sc.nextInt();
+        }
+
+
+        System.out.println("Muito obrigado\n");
+
+        System.out.println("Quantidade de combustíveis escolhidos:");
+        System.out.println("Álcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+    }
+
+}
