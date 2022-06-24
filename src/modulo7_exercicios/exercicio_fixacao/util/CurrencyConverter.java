8ea4bb09967fd78e53dad38564050c651f8085df
ApoloGrecco <apolo.argolo@gmail.com>
2022-06-24 19:01:37 -0300

    Módulo 7 - Introdução a Programação à Orientada a Objetos

---
+++ src/modulo7_exercicios/exercicio_fixacao/util/CurrencyConverter.java
@@ -0,0 +1,9 @@
+package modulo7_exercicios.exercicio_fixacao.util;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 0.06;
+    public static double Calculator(double dollar, double quantity){
+        return ( dollar * quantity ) + ( ( dollar * quantity ) * IOF );
+    }
+}
